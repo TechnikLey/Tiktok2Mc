fcc5b59c42f61582b7c123060f5ed4cf26f7e39d
TechnikLey <katzeen123456@gmail.com>
2026-05-24 01:41:27 +0200

    bumpt Tool_verison and updater_version

--- build.py
+++ build.py
@@ -36,8 +36,8 @@ def main():
         # ----- Configuration -----
         MAX_THREADS = 8
         MAX_COPY_THREADS = 16
-        TOOL_VERSION = "v0.4.0"
-        UPDATER_VERSION = "v1.3.0"
+        TOOL_VERSION = "v0.5.0"
+        UPDATER_VERSION = "v1.4.0"
 
         IS_WINDOWS = sys.platform == "win32"
         SUFFIX = ".exe" if IS_WINDOWS else ".bin"
